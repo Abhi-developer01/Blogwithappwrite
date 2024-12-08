da3a7ac877568e940542320e47a9670dad4e97af
Abhi-developer01 <theabhishek96@gmail.com>
2024-12-08 23:18:02 +0530

    import naming changes

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -12,6 +12,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
+            apiKey="jgr7yzbfy2q0j7nz8l144md5s66kxnol78uw6tfj5x1skcpi"
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
